44b501592dcc8229976f9b29720555679bd70041
MedhaK24 <86275772+MedhaK24@users.noreply.github.com>
2022-09-13 19:55:03 -0700

    Add files via upload

---
+++ Class102/OSModule.py
@@ -0,0 +1,8 @@
+import os 
+#print (os.getcwd())
+#os.mkdir('/Users/mk24/Downloads/Python/Nidhi')
+#print(os.listdir('/Users/mk24/Downloads/Python'))
+#print(os.path.exists('/Users/mk24/Downloads/Python/vidhi'))
+root,ext=os.path.splitext('OSModule.py')
+print (root)
+print(ext) 
---
+++ Class102/Shutilmodule.py
@@ -0,0 +1,4 @@
+import shutil
+#shutil.copy('file1.txt','test')
+#shutil.move('file2.txt','test')
+shutil.move('test/file2.txt','/Users/mk24/Downloads/Python/Nidhi')
